6ef9bda3e2505360fe18301fbad6a588fc9ffecb
HIDORAKAI002 <josephnikhil916@gmail.com>
2026-04-27 20:06:07 +0530

    chore(sync): Update infiniflow/ragflow

--- vector_databases___rag/rag_frameworks___tools/ragflow/web/src/utils/api.ts
+++ vector_databases___rag/rag_frameworks___tools/ragflow/web/src/utils/api.ts
@@ -119,11 +119,11 @@ export default {
     `${restAPIv1}/datasets/${datasetId}/documents`,
   documentRename: (datasetId: string, documentId: string) =>
     `${restAPIv1}/datasets/${datasetId}/documents/${documentId}`,
+  documentIngest: `${restAPIv1}/documents/ingest`,
   documentCreate: (datasetId: string) =>
     `${restAPIv1}/datasets/${datasetId}/documents?type=empty`,
-  documentRun: `${webAPI}/document/run`,
   documentChangeParser: `${webAPI}/document/change_parser`,
-  documentThumbnails: `${webAPI}/document/thumbnails`,
+  documentThumbnails: `${restAPIv1}/thumbnails`,
   getDocumentFile: `${webAPI}/document/get`,
   getDocumentFileDownload: (docId: string) =>
     `${webAPI}/document/download/${docId}`,
