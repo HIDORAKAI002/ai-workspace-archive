13266a1e573a213c18d00e26a6f61b74090fcca2
HIDORAKAI002 <josephnikhil916@gmail.com>
2026-04-29 17:32:07 +0530

    chore(sync): Update vectorize-io/hindsight

--- ai_agents/memory_systems/hindsight/hindsight-docs/sidebars.ts
+++ ai_agents/memory_systems/hindsight/hindsight-docs/sidebars.ts
@@ -276,6 +276,18 @@ const sidebars: SidebarsConfig = {
         },
         {
           type: 'link',
+          href: '/sdks/integrations/agentcore',
+          label: 'AgentCore Runtime',
+          customProps: { icon: '/img/icons/agentcore.png' },
+        },
+        {
+          type: 'link',
+          href: '/sdks/integrations/smolagents',
+          label: 'SmolAgents',
+          customProps: { icon: '/img/icons/smolagents.png' },
+        },
+        {
+          type: 'link',
           href: '/sdks/integrations/ag2',
           label: 'AG2',
           customProps: { icon: '/img/icons/ag2.svg' },
