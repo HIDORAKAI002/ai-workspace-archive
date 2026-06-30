9bbf89ccef29a83548c47b4aa2e84d0fafbaf777
HIDORAKAI002 <josephnikhil916@gmail.com>
2026-07-01 00:37:38 +0530

    chore(sync): Update MCP TypeScript SDK

---
+++ mcps/official_anthropic/typescript-sdk/docs/.vitepress/theme/Banner.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="version-banner">
+        Looking for the <a href="https://ts.sdk.modelcontextprotocol.io/" target="_self">v1 documentation</a>?
+    </div>
+</template>
---
+++ mcps/official_anthropic/typescript-sdk/docs/v1/.vitepress/theme/Banner.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="version-banner">
+        Looking for the <a href="https://ts.sdk.modelcontextprotocol.io/v2/" target="_self">v2 beta documentation</a>?
+    </div>
+</template>
