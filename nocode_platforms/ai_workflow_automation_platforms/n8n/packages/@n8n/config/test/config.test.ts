06bdff002f754c6533ec7ec1b157804128450542
HIDORAKAI002 <josephnikhil916@gmail.com>
2026-07-02 17:40:42 +0530

    chore(sync): Update n8n

--- nocode_platforms/ai_workflow_automation_platforms/n8n/packages/@n8n/config/test/config.test.ts
+++ nocode_platforms/ai_workflow_automation_platforms/n8n/packages/@n8n/config/test/config.test.ts
@@ -194,7 +194,6 @@ describe('GlobalConfig', () => {
 			disabled: false,
 			path: 'api',
 			swaggerUiDisabled: false,
-			packagesEnabled: false,
 		},
 		templates: {
 			enabled: true,
@@ -256,6 +255,8 @@ describe('GlobalConfig', () => {
 				includeWorkflowInfoMetrics: false,
 				workflowInfoMetricInterval: 60,
 				includeDbPoolMetrics: false,
+				includeWorkflowPublicationMetrics: false,
+				workflowPublicationMetricInterval: 60,
 			},
 			additionalNonUIRoutes: '',
 			disableProductionWebhooksOnMainProcess: false,
